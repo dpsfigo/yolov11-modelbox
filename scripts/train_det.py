00730a0376d3996e3bb518e5f13f12e595179412
dpsfigo <dps_figo@163.com>
2025-09-28 17:26:53 +0800

    feat:删除多余代码

--- scripts/train_det.py
+++ scripts/train_det.py
@@ -2,7 +2,7 @@
 Author: dpsfigo
 Date: 2024-11-23 13:34:24
 LastEditors: dpsfigo
-LastEditTime: 2025-08-25 17:54:28
+LastEditTime: 2025-09-28 17:19:41
 Description: 请填写简介
 '''
 import os
@@ -15,8 +15,17 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolo11n-pose.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+# model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n-pose.yaml").load("yolo11n-pose.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=1, imgsz=640, device="cpu")
+model = YOLO("/Users/qingyuan/cv/ultralytics/ultralytics/cfg/models/11/yolo11-MobileNetV1.yaml")
+model.load('yolo11n.pt') 
+model.train(data='coco8.yaml', 
+            imgsz=640,
+            device='cpu',
+            epochs=10,
+            batch=4,
+            optimizer='SGD',
+            amp=False
+            )
